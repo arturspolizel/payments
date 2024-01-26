4d20fab5a0c29a29e7be3d8c5d396ed141e5f053
Artur Polizel <artur.polizel+github@gmail.com>
2024-01-26 11:09:35 -0300

    Adding create success test

--- controller/payment_test.go
+++ controller/payment_test.go
@@ -17,19 +17,45 @@ var mockedPayment = model.Payment{
 	Currency:   model.USD,
 }
 
+var paymentFromRequest = model.Payment{
+	MerchantId: "1",
+	Amount:     100,
+	Tips:       100,
+	Total:      200,
+	Currency:   model.USD,
+}
+
+var paymentWithTotal = model.Payment{
+	MerchantId: "1",
+	Amount:     100,
+	Tips:       100,
+	Total:      200,
+	Currency:   model.USD,
+}
+
 func TestGet(t *testing.T) {
 	assert := assert.New(t)
 	mockRepo := mocks.NewPaymentRepository(t)
 
 	mockRepo.Mock.On("Get", uint(1)).Return(mockedPayment)
+	defer mockRepo.AssertExpectations(t)
 
 	paymentController := NewPaymentController(mockRepo)
 	returnPayment := paymentController.Get(1)
 
 	mockRepo.AssertExpectations(t)
-	assert.Equal(returnPayment, mockedPayment)
+	assert.Equal(mockedPayment, returnPayment)
 }
 
 func TestCreate(t *testing.T) {
+	assert := assert.New(t)
+	mockRepo := mocks.NewPaymentRepository(t)
 
+	mockRepo.Mock.On("Create", paymentWithTotal).Return(uint(1))
+	defer mockRepo.AssertExpectations(t)
+
+	paymentController := NewPaymentController(mockRepo)
+	id := paymentController.Create(paymentFromRequest)
+
+	assert.Equal(uint(1), id)
 }
